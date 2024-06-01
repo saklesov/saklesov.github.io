558dc3c1266d1feee46db1e858cd2aa1755403c7
saklesov <74864891+saklesov@users.noreply.github.com>
2024-06-01 20:17:38 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// script.js
 
+// Add event listeners to buttons
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        // Add functionality here
+    });
+});
